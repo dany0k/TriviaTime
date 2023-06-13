f00234c90cd6e1edf56cda586c69ef93a0cf4fec
dany0k <zmaev.vsu@gmail.com>
2023-06-13 08:40:11 +0300

    Update design

--- TriviaTime/app/src/main/java/ru/vsu/cs/zmaev/MainActivity.java
+++ TriviaTime/app/src/main/java/ru/vsu/cs/zmaev/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home,
                 R.id.navigation_account,
@@ -35,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.resultFragment
         ).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
